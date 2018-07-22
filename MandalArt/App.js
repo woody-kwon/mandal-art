8b8d1c981351fc924bfc244802f2a2b457b9afd7
woody <qmui82@gmail.com>
2018-07-22 16:13:01 +0900

    modified:   MandalArt/App.js

--- MandalArt/App.js
+++ MandalArt/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-      <img src={require('./images/mandalart_logo.png')} />
+        <Image src={require('./images/mandalart_logo.png')} />
       </View>
     );
   }
